200051a179b20340e7da39e47f5ca5b036a2709f
Egor213 <zamok7707@gmail.com>
2025-02-14 19:54:16 +0300

    Добавлены методы поиска постов по заданным параметрам. Созданы тестовые множества для демонстрации работы класса.

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -32,6 +32,31 @@ class Neo4jDatabase:
                 relation_name
             )
 
+    def find_posts(self, **kwargs):
+        valid_keys = {'title', 'author', 'id'}
+        if not valid_keys.intersection(kwargs.keys()) and len(kwargs):
+            return 'Некорректные параметры'
+        with self.driver.session() as session:
+            return session.read_transaction(self.__find_posts_tx, **kwargs)
+        
+
+    @staticmethod
+    def __find_posts_tx(tx, **kwargs):
+        query = 'MATCH (n:Post) '
+        if kwargs:
+            query += 'WHERE '
+        find_params = list()
+        if 'id' in kwargs:
+            find_params.append(f'id(n)=$id')
+        if 'author' in kwargs:
+            find_params.append(f'n.author=$author')
+        if 'title' in kwargs:
+            find_params.append(f'n.title=$title')
+        query += ' AND '.join(find_params)
+        query += ' RETURN n'
+        result = tx.run(query, **kwargs)
+        return [record['n'] for record in result] 
+
     @staticmethod
     def _create_post_tx(tx, title, text, author):
         query = """
@@ -56,16 +81,55 @@ class Neo4jDatabase:
         )
 
 
+def id_node(node):
+    return node.element_id.split(':')[2]
+
+
+TEST_DATA_POSTS = (
+    ('Пост о природе', 'Много информации о природе', 'Василий'),
+    ('Рецепт здорового питания', 'Как готовить вкусно и полезно', 'Елена'),
+    ('Технологии будущего', 'Как AI меняет мир', 'Иван'),
+    ('Путеводитель по Токио', 'Лучшие места для посещения в Токио', 'Алексей'),
+    ('Советы по саморазвитию', 'Как стать лучшей версией себя', 'Мария'),
+    ('Как стать успешным в бизнесе', 'Шаги для начинающего предпринимателя', 'Дмитрий'),
+    ('Спортивные тренировки для начинающих', 'Как начать тренироваться без травм', 'Светлана'),
+    ('Финансовая грамотность', 'Как управлять своими деньгами', 'Ольга'),
+    ('Психология межличностных отношений', 'Как строить здоровые отношения', 'Никита'),
+    ('Путеводитель по Парижу', 'Топ-10 мест для посещения в Париже', 'Василий')
+)
+
+
+TEST_FIND_POSTS = (
+    {},
+    {'author': 'Василий'},
+    {'id': 1},
+    {'title': 'Рецепт здорового питания'},
+    {'author': 'Василий', 'title': 'Путеводитель по Парижу'},
+)
+
+TEST_RELATION_POSTS = (
+    (1, 2, 'one-to-two'),
+    (1, 3, 'one-to-three'),
+    (3, 4, 'three-to-four')
+)
+
 def main():
     load_dotenv()
     uri = f'neo4j://localhost:{os.getenv("PORT", "7687")}'
     username = os.getenv('NEO4J_USER', 'neo4j')
     password = os.getenv('PASSWORD')
     database = Neo4jDatabase(uri, username, password)
-    post1 = database.create_post("test1", 'test1', 'test1')
-    post2 = database.create_post("test2", 'test2', 'test2')
-    database.create_relation(post1, post2, 'TEEEST')
-    print("Подключение к Neo4j установлено!")
+    for post in TEST_DATA_POSTS:
+        database.create_post(*post)
+
+    for find_params in TEST_FIND_POSTS:
+        nodes = database.find_posts(**find_params)
+        for node in nodes:
+            print(f"Title: {node['title']}, Text: {node['text']}, Author: {node['author']}")
+
+    for relation in TEST_RELATION_POSTS:
+        database.create_relation(*relation)
+        
     database.close()
 
 
