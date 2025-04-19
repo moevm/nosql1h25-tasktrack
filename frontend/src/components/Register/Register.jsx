0c5350043c255288884c4cc230bad8d615997972
Egor213 <zamok7707@gmail.com>
2025-04-19 12:45:49 +0300

    add register page without error handler and validators

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,47 +1,47 @@
-// Register.jsx
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './Register.css'; 
+import './Register.css';
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleRegister = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
     console.log("Email:", email);
     console.log("Password:", password);
     navigate("/login");
   };
 
   return (
-    <div className="main-content">
-      <h1>Регистрация</h1>
-      <div className="form-group">
-        <label htmlFor="email">Электронная почта</label>
-        <input
-          type="email"
-          id="email"
-          className="form-control"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Введите ваш email"
-        />
+    <div className="register-container">
+      <div className="register-box">
+        <h2>Регистрация</h2>
+        <form onSubmit={handleRegister}>
+            <label htmlFor="email">Электронная почта</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Введите ваш email"
+              required
+            />
+            <label htmlFor="password">Пароль</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Введите ваш пароль"
+              required
+            />
+          <button type="submit">
+            Зарегистрироваться
+          </button>
+        </form>
       </div>
-      <div className="form-group">
-        <label htmlFor="password">Пароль</label>
-        <input
-          type="password"
-          id="password"
-          className="form-control"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Введите ваш пароль"
-        />
-      </div>
-      <button className="btn btn-primary" onClick={handleRegister}>
-        Зарегистрироваться
-      </button>
     </div>
   );
 }
