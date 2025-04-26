5f3ad74442efc3543e7ae50c37588fa9e7097482
Egor213 <zamok7707@gmail.com>
2025-04-26 19:04:41 +0300

    add check authenticated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,24 @@ import {
   Routes,
   Navigate,
 } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Dashboard from './Dashboard';
 import Settings from './Settings';
 import LoginPage from './components/Login/LoginPage';
 import Register from './components/Register/Register';
 import GuestNav from './components/Routers/GuestNav/GuestNav';
 import UserNav from './components/Routers/UserNav/UserNav';
+import PrivateRoute from './PrivateRoute';
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    () => localStorage.getItem('isAuthenticated') === 'true'
+  );
+
+  useEffect(() => {
+    localStorage.setItem('isAuthenticated', isAuthenticated);
+  }, [isAuthenticated]);
+
   function Layout({ children, isAuthenticated }) {
     return (
       <div>
@@ -28,18 +36,36 @@ export default function App() {
       </div>
     );
   }
+
   return (
     <Router>
       <Layout isAuthenticated={isAuthenticated}>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/" element={<Navigate to={isAuthenticated ? "/tasks" : "/login"} />} />
           <Route
             path="/login"
             element={<LoginPage setIsAuthenticated={setIsAuthenticated} />}
           />
-          <Route path="/tasks" element={<Dashboard />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+          <Route
+            path="/tasks"
+            element={
+              <PrivateRoute isAuthenticated={isAuthenticated}>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <PrivateRoute isAuthenticated={isAuthenticated}>
+                <Settings />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </Layout>
     </Router>
