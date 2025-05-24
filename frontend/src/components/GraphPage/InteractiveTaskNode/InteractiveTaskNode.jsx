e913045e95772626c2cf441bc44260b0d3a44ba1
Egor213 <zamok7707@gmail.com>
2025-05-24 15:53:46 +0300

    add connections

--- frontend/src/components/GraphPage/InteractiveTaskNode/InteractiveTaskNode.jsx
+++ frontend/src/components/GraphPage/InteractiveTaskNode/InteractiveTaskNode.jsx
@@ -1,5 +1,6 @@
 // InteractiveTaskNode.jsx
 import React from 'react';
+import { Handle, Position } from 'reactflow';
 
 const InteractiveTaskNode = ({ data }) => {
   return (
@@ -12,15 +13,22 @@ const InteractiveTaskNode = ({ data }) => {
         color: '#fff',
         width: '200px',
         cursor: 'pointer',
+        position: 'relative', // обязательно для Handle
       }}
     >
+      {/* Вход (сверху) */}
+      <Handle type="target" position={Position.Top} id="top" style={{ background: '#555' }} />
+      
+
       <strong>{data.label}</strong>
       <br />
       <small>Дедлайн: {data.deadline || 'Нет'}</small> <br />
       <small>Статус: {data.status || 'Нет'}</small> <br />
       <small>Приоритет: {data.priority || 'Нет'}</small> <br />
       <small>Группа: {data.group || 'Нет'}</small>
-      <br />
+
+      {/* Выход (снизу) */}
+      <Handle type="source" position={Position.Bottom} id="bottom" style={{ background: '#999' }} />
     </div>
   );
 };
